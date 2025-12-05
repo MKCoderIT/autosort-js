0dc72b8556e8bdaea60f9a5ec76502f135858fdf
Kamyar taslimi <kamyar.taslimi.1385@gmail.com>
2025-12-05 10:54:39 +0330

    refactor: restructure insertionSort to validate input before processing

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -3,21 +3,20 @@ import { ErrorsCall } from "../core/analyzeArray.js";
 
 export function insertionSort(array, ascending = true, func = null) {
     const compare = func || autoCompare;
-    const n = array.length;
+    if (ErrorsCall.confirmCompare(array, compare)) {
+        const n = array.length;
 
-    for (let i = 0; i < n - 1; i++) {
-        const key = array[i + 1];
-        let cart = i;
+        for (let i = 0; i < n - 1; i++) {
+            const key = array[i + 1];
+            let cart = i;
 
-        while (cart >= 0 && ascending ? compare(array[cart], key) > 0 : compare(array[cart] , key) < 0) {
-            array[cart + 1] = array[cart];
-            cart--;
+            while (cart >= 0 && (ascending ? compare(array[cart], key) > 0 : compare(array[cart], key) < 0)) {
+                array[cart + 1] = array[cart];
+                cart--;
+            }
+            array[cart + 1] = key;
         }
-        array[cart + 1] = key;
+        return array;
     }
-    return array;
+    return false;
 }
-
-const test = [4, "bahar", 2, "ali", 3, 1];
-insertionSort(test , false);
-console.log(test);
