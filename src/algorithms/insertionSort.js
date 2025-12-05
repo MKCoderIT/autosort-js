4a9d0d8dee129d6371d8836b611059e2be8eee85
Kamyar taslimi <kamyar.taslimi.1385@gmail.com>
2025-12-05 10:36:31 +0330

    refactor: enhance insertionSort to use auto comparison function

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -2,13 +2,14 @@ import { autoCompare } from "../core/autoCompare.js";
 import { ErrorsCall } from "../core/analyzeArray.js";
 
 export function insertionSort(array, ascending = true, func = null) {
+    const compare = func || autoCompare;
     const n = array.length;
 
     for (let i = 0; i < n - 1; i++) {
         const key = array[i + 1];
         let cart = i;
 
-        while (cart >= 0 && array[cart] > key) {
+        while (cart >= 0 && ascending ? compare(array[cart], key) > 0 : compare(array[cart] , key) < 0) {
             array[cart + 1] = array[cart];
             cart--;
         }
@@ -17,6 +18,6 @@ export function insertionSort(array, ascending = true, func = null) {
     return array;
 }
 
-const test = [4, 2 ,3, 1];
-insertionSort(test);
+const test = [4, "bahar", 2, "ali", 3, 1];
+insertionSort(test , false);
 console.log(test);
