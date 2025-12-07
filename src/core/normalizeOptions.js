cfac1ef5f2e5bacd536324f4eea36d61585733c8
Kamyar taslimi <kamyar.taslimi.1385@gmail.com>
2025-12-08 00:30:08 +0330

    docs: remove redundant inline docs

--- src/core/normalizeOptions.js
+++ src/core/normalizeOptions.js
@@ -2,14 +2,6 @@ import { autoCompare } from "./autoCompare.js";
 import { Validators } from "./validators.js";
 
 const DEFAULT_OPTIONS = { ascending: true, compare: null };
-
-/**
- * Merge options + validate + return normalized config.
- * @template T
- * @param {T[]} array
- * @param {{ ascending?: boolean, compare?: ((a:T,b:T)=>number) | null }} [options]
- * @returns {{ ascending: boolean, compare: (a:T,b:T)=>number }}
- */
 export function normalizeSortOptions(array, options = {}) {
     const mergedOptions = { ...DEFAULT_OPTIONS, ...options };
     const ascending = mergedOptions.ascending;
